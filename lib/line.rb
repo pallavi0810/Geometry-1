2a9e330481abaddcb3e432dc9d501d95fe953213
pallavi0810 <rupai_810@yahoo.co.in>
2015-06-02 14:50:05 +0530

    Line comparison using operator overloading

--- lib/line.rb
+++ lib/line.rb
@@ -1,6 +1,6 @@
 class Line
 	#class to define coordinates of a line
-	
+	attr_reader :x1, :x2, :y1, :y2
 	def initialize(x1,y1,x2,y2)
 		@x1 = x1
 		@y1 = y1
@@ -10,8 +10,8 @@ class Line
 	def to_string
         "Line (#{@x1}, #{@x2}, #{@y1}, #{@y2})"
     end
-	def self.equals(line1,line2)
-		if line1.to_string == line2.to_string
+	def equals(line2)
+		if (self.x1 == line2.x1 && self.x2 == line2.x2 && self.y1 == line2.y1 && self.y2 == line2.y2) || (self.x1 == line2.x2 && self.x2 == line2.x1 && self.y1 == line2.y2 && self.y2 == line2.y1) 
 			true
 		else 
 			false
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -4,7 +4,19 @@ describe 'Compare two lines for equality' do
 
 	 it "compare two lines to be equal" do
 	 		line1 = Line.new(2,3,5,7)
-	 		line2 = Line.new(2,3,5,9)
-	 		expect(Line.equals(line1,line2)).to eq(true)
+	 		line2 = Line.new(2,3,5,7)
+	 		expect(line1.equals(line2)).to eq(true)
+	 end
+
+	 it "compare two lines to be unequal" do
+	 		line1 = Line.new(2,3,5,7)
+	 		line2 = Line.new(2,6,5,7)
+	 		expect(line1.equals(line2)).to eq(false)
+	 end
+
+	 it "compare two lines to be equal with start point of one line equal to end point of other and wise versa" do
+	 		line1 = Line.new(2,3,5,7)
+	 		line2 = Line.new(5,7,2,3)
+	 		expect(line1.equals(line2)).to eq(true)
 	 end
 end
