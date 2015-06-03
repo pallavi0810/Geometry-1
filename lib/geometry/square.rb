527c01e5c33ad999ee5a1dfb6c7c79d039b92cfd
pallavi0810 <rupai_810@yahoo.co.in>
2015-06-03 11:19:02 +0530

    removed attr_accessor

--- lib/geometry/square.rb
+++ lib/geometry/square.rb
@@ -1,18 +1,17 @@
 # Represents properties and operations related to a square
 
 class Geometry::Square
-	attr_accessor :side, :area, :perimeter, :point
 	def initialize(side, point)
 		@side = side
 		@point = point
 	end
 
 	def area
-		area = side*side
+		area = side * side
 	end
 
 	def perimeter
-		perimeter = 4*side
+		perimeter = 4 * side
 	end
 
 end
